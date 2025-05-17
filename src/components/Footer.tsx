cf940ce1b11b2ca2764f7a682cbcab40ff7eaafe
[dyad] <git@dyad.sh>
2025-05-17 16:05:47 +0545

    [dyad] Added a footer section - wrote 2 file(s)

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white py-6 text-center">
+      <div className="max-w-7xl mx-auto px-4 md:px-8 lg:px-16">
+        <p className="text-sm">
+          © Copyright 2025 by Bikash Gupta
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,16 +3,19 @@ import HeroSection from "@/components/HeroSection";
 import AboutSection from "@/components/AboutSection";
 import ProjectsSection from "@/components/ProjectsSection";
 import ContactFormSection from "@/components/ContactFormSection";
+import Footer from "@/components/Footer"; // Import the new Footer component
 
 const Index = () => {
   return (
-    <div className="min-h-screen bg-[#f5f5f5]"> {/* Set background color */}
+    <div className="min-h-screen bg-[#f5f5f5] flex flex-col"> {/* Added flex-col to push footer down */}
       <Header />
       <HeroSection />
       <AboutSection />
       <ProjectsSection />
       <ContactFormSection />
       {/* Add other sections here later */}
+      <div className="flex-grow"></div> {/* Added flex-grow to push footer to the bottom */}
+      <Footer /> {/* Add the Footer component */}
     </div>
   );
 };
